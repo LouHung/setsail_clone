35823614885019710a5a4ca8a1db69748d9654b7
louhung <quochung51199@gmail.com>
2021-12-10 20:47:29 +0700

    done home page

--- js/base.js
+++ js/base.js
@@ -4,10 +4,10 @@ const navBtn = $('.header-nav-lookup_btn-nav')
 const searchBtn = $('.header-nav-lookup_btn-lookup')
 const searchInput = $('.header-nav__search-input')
 const closeNavBtn = $('.header-nav-aside_closeBtn')
+const btnMoveTop = $('.btn-movetop')
 
 
 searchBtn.onclick = function(){
-    console.log('aa')
     searchInput.classList.toggle('active')
 }
 navBtn.onclick = function(){
@@ -18,4 +18,55 @@ closeNavBtn.onclick = function(){
     $('.header-nav-aside_container').classList.remove('active')
     $('.header-nav-aside__black-slide').classList.remove('active')
 
+}
+
+
+// nav move
+function runNavbar() {
+  if ( document.documentElement.scrollTop > 30) {
+    console.log("Scroll")
+    $('.header-nav-container').classList.add('active');
+    $('.header-nav-wrap').style.padding = '25px'
+    btnMoveTop.style.display='flex'
+  }else{
+    $('.header-nav-container').classList.remove('active');
+    $('.header-nav-wrap').style.padding = '25px 25px 40px 25px'
+    btnMoveTop.style.display='none'
+
+  }
+  
+}
+if( window.matchMedia("(max-width: 478px)").matches){
+    $('.header-nav-wrap').style.padding = '5px 25px'
+
+  }else{
+    $('.header-nav-wrap').style.padding = '25px 25px 40px 25px'
+
+    window.onscroll = function() {runNavbar()};
+
+  }
+
+window.addEventListener('resize', function(){
+    let width = document.body.clientWidth
+    if(width > 478){
+    $('.header-nav-wrap').style.padding = '25px 25px 40px 25px'
+
+        window.onscroll = function() {runNavbar()};
+
+    }else{
+        $('.header-nav-wrap').style.padding = '5px 25px'
+
+        window.onscroll = function() {
+            $('.header-nav-wrap').style.padding = '5px 25px'
+        };
+    }
+})
+
+// btn move top
+
+function scrollTop(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+btnMoveTop.onclick =function(){
+    scrollTop()
 }
--- js/home.js
+++ js/home.js
@@ -1,2 +1,6 @@
 
+const filterBtn= $('.body-filter__service-item')
 
+if(filterBtn.classList.contains('active')){
+    $('.body-filter__service-item--text').style.color='white'
+}

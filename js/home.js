3ab2e8fee625edad8cc2f7e15f2afb782ae455b5
louhung <quochung51199@gmail.com>
2021-12-12 21:02:07 +0700

    --m fix bug

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
 
+const listCards = Array.from($$('.body-filter__service-item')) 
+console.log(listCards)
+
+listCards.forEach(card => {
+   
+    card.onclick= function(){
+        listCards.forEach((card2)=>{
+            if(card2.classList.contains('active')){
+                card2.classList.remove('active')
+        }
+        })
+
+        card.classList.add('active')
+    }
+})
